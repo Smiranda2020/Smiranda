76ee7b7360bb78e30d16b74fddf495e98f10ac1d
santiagoMiranda <santiago.miranda@acciona-it.com.ar>
2020-10-16 17:45:22 -0300

    Uso del swicht

--- Seccion4/Program.cs
+++ Seccion4/Program.cs
@@ -4,9 +4,40 @@ namespace Seccion4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ejemplo uso del switch");
+
+            int dia = 3;
+
+            switch (dia)
+            {
+                case 0:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 1:
+                    Console.WriteLine("Martes");
+                    break;
+                case 2:
+                    Console.WriteLine("Miercoles");
+                    break;
+                case 3:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 4:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 5:
+                    Console.WriteLine("Sabado");
+                    break;
+                case 6:
+                    Console.WriteLine("Domingo");
+                    break;
+                default: Console.WriteLine("El valor es incorrecto");
+                    break;
+            }
+            Console.ReadLine();
+            
         }
     }
 }
