09b837c3e59ef70dab39d1648c79c774d4c5df1f
santiagoMiranda <santiago.miranda@acciona-it.com.ar>
2020-10-17 14:37:44 -0300

    operador de accinaccion Compuesto

---
+++ Seccion5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace Seccion5
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            //Operadores de accinacion compuesta
+            string texto = "Programando ";
+            Console.WriteLine(texto);
+            texto += "con C#";
+            Console.WriteLine(texto);
+            string texto2 = "y .Net";
+            texto += texto2;
+            Console.WriteLine(texto);
+            Console.ReadLine();
+        }
+    }
+}
